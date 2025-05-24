1922d72ebf6a7734071d1b7f50ede66d6b8ae2ce
Seifii <sofiapetrova893@gmail.com>
2025-05-24 14:54:33 +0300

    2

--- Cat.py
+++ Cat.py
@@ -1,9 +1,9 @@
-w = list(map(int, input().split()))
-N = w[0]
-weights = w[1]
+wh = list(map(int, input().split()))
+N = wh[0]
+w = wh[1:]
 min_w = min(w)
 max_w = max(w)
-sum_w = (sum(w) - w[0])
+sum_w = sum(w)
 print(max_w)
 print(min_w)
 print(sum_w)
